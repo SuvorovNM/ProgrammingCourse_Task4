46789c9c4e134d022ced483828fe393780c09c8d
SuvorovNM <SuvorovNM@gmail.com>
2018-05-22 13:56:31 +0500

    Comments

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -10,25 +10,27 @@ namespace Task4
     {
         static void Main(string[] args)
         {
-            double eps,summ=0;
-            bool OK;
+            double eps,summ=0;//eps - заданная точность, summ - итоговая сумма
+            bool OK=true;//Проверка корректности ввода
             do
             {
+                if (!OK) Console.WriteLine("Ошибка ввода!");
                 Console.WriteLine("Введите точность для нахождения суммы (<0,1): ");
                 OK = Double.TryParse(Console.ReadLine(), out eps);
-            } while (!OK||eps>0.1||eps<0);
+            } while (!OK||eps>0.1||eps<=0);
             int i = 1;
             bool inf;
             do
             {
-                summ += 1 / (double)(i * (i + 1));
+                summ += 1 / (double)(i * (i + 1));//Добавление элемента к сумме
                 i++;
-                inf = Double.IsInfinity(summ);
-            } while (!inf&&(Math.Abs((1 / (double)(i * (i + 1)))) > eps));
-            if (!inf)
+                inf = Double.IsInfinity(summ);//Проверка на то, стала ли сумма равна бесконечности
+            } while (!inf&&(Math.Abs((1 / (double)(i * (i + 1)))) > eps));// Если сумма = бесконечности или следующий элемент < eps, то выход
+            if (!inf)//Если сумма не бесконечность, то вывод результата
             {
                 Console.WriteLine("Результат: " + summ);
             }
+            //Иначе вывод сообщения об ошибке
             else Console.WriteLine("Невозможно вычислить результат!");
             Console.Read();
         }
